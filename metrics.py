551285fe34804f4ef68fff3bb5532dcd167c007e
Bruno Guilhem <boguilhem@gmail.com>
2023-03-04 14:21:17 -0300

    feat: calculating root-mean square error

--- metrics.py
+++ metrics.py
@@ -1,5 +1,19 @@
 import cv2
+import math
 import numpy as np
+
+##############
+#    DEFS    #
+##############
+
+def rmse(result_image, ground_truth):
+    result_image[np.isnan(result_image)] = 0
+    ground_truth[np.isnan(ground_truth)] = 0
+
+    rmse = math.sqrt((np.square(result_image - ground_truth)).mean())
+    
+    return rmse
+
 
 ##############
 #   PARAMS   #
@@ -10,6 +24,7 @@ img_out = cv2.imread('out/cones_5_robust.png').astype(float)
 
 # Max error that is forgiven
 GRACE = 5
+
 
 ##############
 #    MAIN    #
@@ -50,3 +65,7 @@ print(f"Bad pixel percentage: {err_count*100 / (WIDTH*HEIGHT):.2f}%")
 print(f"Saving error map as errors.png...")
 cv2.imwrite('errors.png', img_diff)
 print("Done!")
+
+
+# Calculating Root-Mean square error
+print(f"Root-Mean Square Error: {rmse(img_out, img_ground)}")
